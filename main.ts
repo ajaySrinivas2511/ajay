2a8bf4a995855ae9d58bfde36e91628219326ef4
ajaySrinivas2511 <106160251+ajaySrinivas2511@users.noreply.github.com>
2022-05-24 18:09:07 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+namespace SpriteKind {
+    export const bafellow = SpriteKind.create()
+}
+babablacksheep += 1
+let babablacksheep: number = sprites.create(img`
+    2 2 2 . f f f f . 2 2 2 . 
+    2 2 2 f f f f f 2 2 2 2 . 
+    2 2 2 f f f f c 2 2 2 2 . 
+    2 2 2 f f f c c 2 2 2 2 . 
+    2 2 2 c f f f f 2 2 2 2 . 
+    2 c c f f f e e f f c 2 . 
+    2 2 2 2 f e e f 2 2 2 2 . 
+    f f f 2 f e e f 2 f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.bafellow)
+music.playMelody("C5 C C5 C5 C5 C5 D C5 ", 120)
